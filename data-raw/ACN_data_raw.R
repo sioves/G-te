3e338e8b6eaf772d0023d7a7cac6ffad2299a0bd
anban2 <andreaandersen@clin.au.dk>
2026-03-18 13:56:55 +0100

    added biloat and bil drink data raw

--- data-raw/ACN_data_raw.R
+++ data-raw/ACN_data_raw.R
@@ -17,3 +17,44 @@ bilpow_acn <- data.frame(
   ),
   Replicate = rep(1:2, 7)
 )
+
+
+# Bilberry drink
+bil_acn <- data.frame(
+  Temperature = c(
+    "O", "O",
+    "RT", "RT", "RT", "RT", "RT", "RT",
+    "8C", "8C", "8C", "8C", "8C", "8C"
+  ),
+  Time = c(
+    "0", "0",
+    "4W", "4W", "3M", "3M", "8M", "8M",
+    "4W", "4W", "3M", "3M", "8M", "8M"
+  ),
+  Value = c(
+    4.65, 4.44,
+    4.11, 3.72, 3.25, 2.81, 2.70, 2.79,
+    4.22, 4.75, 3.65, 3.83, 3.91, 4.10
+  ),
+  Replicate = rep(1:2, 7)
+)
+
+# Bilberry Oat drink
+biloat_acn <- data.frame(
+  Temperature = c(
+    "O", "O",
+    "RT", "RT", "RT", "RT", "RT", "RT",
+    "8C", "8C", "8C", "8C", "8C", "8C"
+  ),
+  Time = c(
+    "0", "0",
+    "4W", "4W", "3M", "3M", "8M", "8M",
+    "4W", "4W", "3M", "3M", "8M", "8M"
+  ),
+  Value = c(
+    1.59, 1.38,
+    1.04, 1.49, 0.91, 0.92, 0.48, 0.54,
+    1.29, 1.47, 1.28, 1.42, 1.24, 1.32
+  ),
+  Replicate = rep(1:2, 7)
+)
